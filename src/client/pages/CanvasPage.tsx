73bc62155a3b21af30e3ceb8f8e8b82c732ce3ff
cameronapak <cameronandrewpak@gmail.com>
2025-04-14 22:35:24 -0500

    Enhance CanvasPage by integrating custom Tldraw components: add a components prop to Tldraw for better UI customization and include various UI components for future use.

--- src/client/pages/CanvasPage.tsx
+++ src/client/pages/CanvasPage.tsx
@@ -1,12 +1,42 @@
-import { Tldraw } from "tldraw";
+import { TLUiComponents, Tldraw } from "tldraw";
 import "tldraw/tldraw.css";
 import { Layout } from "../../components/Layout";
+
+// The type here is include only to ensure this example contains all possible ui components,
+const components: Partial<TLUiComponents> = {
+	// ContextMenu: null,
+	// ActionsMenu: null,
+	// HelpMenu: null,
+	// ZoomMenu: null,
+	// MainMenu: null,
+	Minimap: null,
+	// StylePanel: null,
+	// PageMenu: null,
+	// NavigationPanel: null,
+	// Toolbar: null,
+	// KeyboardShortcutsDialog: null,
+	// QuickActions: null,
+	// HelperButtons: null,
+	DebugPanel: null,
+	DebugMenu: null,
+	MenuPanel: null,
+	TopPanel: null,
+	// CursorChatBubble: null,
+	RichTextToolbar: null,
+	// Dialogs: null,
+	// Toasts: null,
+}
 
 export function CanvasPage() {
   return (
     <Layout mainContentClasses="w-full max-w-full !p-0 h-full">
       <div className="tldraw__editor h-full">
-        <Tldraw inferDarkMode className="h-full" persistenceKey="example"></Tldraw>
+        <Tldraw 
+          className="h-full"
+          components={components}
+          inferDarkMode 
+          persistenceKey="example"
+        ></Tldraw>
       </div>
     </Layout>
   );
